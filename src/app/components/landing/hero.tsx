103b81371248bc847db19d19aaf5142603ddfb84
nikhil <nikhildhanda84@gmail.com>
2025-09-12 01:05:19 +0530

    Added: project.

---
+++ src/app/components/landing/hero.tsx
@@ -0,0 +1,24 @@
+export default function Hero(){
+    return(
+        <>
+        <div className="flex flex-row tracking-tight justify-between px-48">
+
+            <div className="flex flex-col gap-12">
+
+                <div className="text-6xl text-zinc-800 font-primary font-medium tracking-tighter">
+                    Converting cold mails <br /> to warm letters.
+                </div>
+                <div className="font-secondary text-zinc-600 text-lg">
+                    we help you convert your emails, from ones which will never have any effect on the people to ones that leave a huge impact and helps you confirm your job title.
+
+                </div>
+
+            </div>
+
+            
+          
+
+        </div>
+        </>
+    )
+}
